35e7623d3c63b6b0f65c555d0ea431c0f3d0f43a
Andrei <b1naryth1ef@gmail.com>
2016-04-23 06:24:50 -0500

    Add basic RateLimiter to handle 429's

--- src/dscord/api/client.d
+++ src/dscord/api/client.d
@@ -2,12 +2,15 @@ module dscord.api.client;
 
 import std.stdio,
        std.array,
-       std.variant;
+       std.variant,
+       std.conv,
+       core.time;
 
 import vibe.http.client,
        vibe.stream.operations;
 
 import dscord.types.all,
+       dscord.api.ratelimit,
        dscord.util.errors;
 
 class APIError : BaseError {
@@ -17,23 +20,29 @@ class APIError : BaseError {
 }
 
 struct U {
-  string url;
+  string  _bucket;
+  string  value;
 
   this(string url) {
     if (url[0] != '/') {
       url = "/" ~ url;
     }
 
-    this.url = url;
+    this.value = url;
   }
 
   U opCall(string url) {
-    this.url ~= "/" ~ url;
+    this.value ~= "/" ~ url;
     return this;
   }
 
   U opCall(Snowflake s) {
     this.opCall(s.toString());
+    return this;
+  }
+
+  U bucket(string bucket) {
+    this._bucket = bucket;
     return this;
   }
 }
@@ -74,14 +83,24 @@ class APIResponse {
   @property string content() {
     return this.res.bodyReader.readAllUTF8();
   }
+
+  string header(string name, string def="") {
+    if (name in this.res.headers) {
+      return this.res.headers[name];
+    }
+
+    return def;
+  }
 }
 
 class APIClient {
-  string baseURL = "https://discordapp.com/api/";
-  string token;
+  string       baseURL = "https://discordapp.com/api/";
+  string       token;
+  RateLimiter  ratelimit;
 
   this(string token) {
     this.token = token;
+    this.ratelimit = new RateLimiter;
   }
 
   APIResponse requestJSON(HTTPMethod method, U url) {
@@ -92,16 +111,30 @@ class APIClient {
     return requestJSON(method, url, data.dumps());
   }
 
-  APIResponse requestJSON(HTTPMethod method, U url, string data) {
-    auto res = requestHTTP(this.baseURL ~ url.url,
+  APIResponse requestJSON(HTTPMethod method, U url, string data,
+      Duration timeout=15.seconds) {
+
+    // Grab the rate limit lock
+    if (!this.ratelimit.wait(url._bucket, timeout)) {
+      throw new APIError(-1, "Request expired before rate-limit");
+    }
+
+    auto res = new APIResponse(requestHTTP(this.baseURL ~ url.value,
       (scope req) {
         req.method = method;
         req.headers["Authorization"] = this.token;
         req.headers["Content-Type"] = "application/json";
         req.bodyWriter.write(data);
-      });
+    }));
 
-    return new APIResponse(res);
+    // If we got a 429, cooldown and recurse
+    if (res.statusCode == 429) {
+      this.ratelimit.cooldown(url._bucket,
+          dur!"seconds"(res.header("Retry-After", "1").to!int));
+      return this.requestJSON(method, url, data, timeout);
+    }
+
+    return res;
   }
 
   JSONObject me() {
@@ -134,7 +167,8 @@ class APIClient {
       .set!string("nonce", nonce)
       .set!bool("tts", tts);
 
-    auto res = this.requestJSON(HTTPMethod.POST, U("channels")(chan)("messages"), payload);
+    auto res = this.requestJSON(HTTPMethod.POST,
+        U("channels")(chan)("messages").bucket("send-message"), payload);
     res.ok();
     return res.json;
   }
