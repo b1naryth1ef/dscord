54f88d6335fb06af9d4338308d97b067484663f3
Andrei <b1naryth1ef@gmail.com>
2016-09-13 15:33:08 -0500

    Fix incorrect command capture grouping

--- src/dscord/bot/bot.d
+++ src/dscord/bot/bot.d
@@ -216,7 +216,12 @@ class Bot {
     }
 
     // Extract some stuff for the CommandEvent
-    event.contents = strip(capture.post);
+    if (capture.back.length) {
+      event.contents = strip(capture.back);
+    } else {
+      event.contents = strip(capture.post);
+    }
+
     event.args = event.contents.split(" ");
 
     if (event.args.length && event.args[0] == "") {
